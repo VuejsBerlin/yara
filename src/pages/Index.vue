fc223b7f1ce35687ba3e95df4f943cae00e3a053
koehr <n@koehr.in>
2019-04-09 15:00:12 +0200

    init

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <Layout>
+    <h1>YARA</h1>
+    <p>Yet Another Raffle App</p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
